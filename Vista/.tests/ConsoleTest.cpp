2af89d508137df64f9c6cd0091c30df0b2154ac7
Andreas Johansson <glanshammar70@gmail.com>
2024-07-10 06:36:03 +0200

    Added new test variables.

--- Vista/.tests/ConsoleTest.cpp
+++ Vista/.tests/ConsoleTest.cpp
@@ -1,13 +1,16 @@
-#include "../GUI/VConsole.hpp"
+#include <VConsole>
+#include <VString>
 #include "ConsoleTest.hpp"
 
 
 void ConsoleTest(){
     int testValue = 42;
+    VString test = "VString test.";
 
     VDebug(Level::Info, "This is an informational message.");
     VDebug(Level::Warning, "Value might be out of range: " + std::to_string(testValue));
     VDebug(Level::Error, "Critical error occurred!");
     VPrint("Hello world! ", testValue, " Yo mama!");
     VPrint("Bye world! ", testValue * 2, " Yo papa!");
+    VPrint(test);
 }
