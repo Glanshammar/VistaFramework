289e546481cf37f26e5c800f6e9ab0dad4c06af5
Andreas Johansson <glanshammar70@gmail.com>
2024-07-11 17:28:58 +0200

    Added more tests.

--- Vista/.tests/ConsoleTest.cpp
+++ Vista/.tests/ConsoleTest.cpp
@@ -1,16 +1,25 @@
-#include <VConsole>
 #include <VString>
+#include <VConsole>
 #include "ConsoleTest.hpp"
 
+#define vString std::u16string
+
+void Print(const std::u16string& str){
+    for (char16_t ch : str) {
+        std::wcout << (wchar_t)ch;
+    }
+    std::cout << std::endl;
+}
 
 void ConsoleTest(){
     int testValue = 42;
-    VString test = "VString test.";
+    VString test = "VString test. 😊🌍";
+    vString u16str = u"Hello World from u16string! 😊🌍";
+
 
     VDebug(Level::Info, "This is an informational message.");
     VDebug(Level::Warning, "Value might be out of range: " + std::to_string(testValue));
     VDebug(Level::Error, "Critical error occurred!");
-    VPrint("Hello world! ", testValue, " Yo mama!");
-    VPrint("Bye world! ", testValue * 2, " Yo papa!");
-    VPrint(test);
+    std::cout << test << std::endl;
+    Print(u16str);
 }
