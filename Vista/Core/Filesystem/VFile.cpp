59916ee5a0e8a7cc324f92cfa1efab2fc01d5713
Andreas Johansson <glanshammar70@gmail.com>
2024-07-11 17:27:36 +0200

    Removed code for now.

--- Vista/Core/Filesystem/VFile.cpp
+++ Vista/Core/Filesystem/VFile.cpp
@@ -1,16 +1,2 @@
 #include "VFile.hpp"
 
-VFile::VFile(const std::optional<std::string>& filename, const std::optional<std::string>& mode)
-    : filename_(filename), mode_(mode) {
-    cout << "VFile created with filename: " << (filename_ ? *filename_ : "default")
-              << " and mode: " << (mode_ ? *mode_ : "default") << std::endl;
-}
-
-VFile::~VFile() {
-    cout << "VFile destroyed" << std::endl;
-}
-
-void VFile::LoadFile(const string& filePath, const char* mode)
-{
-    FILE* file;
-}
--- Vista/Core/Filesystem/VFile.hpp
+++ Vista/Core/Filesystem/VFile.hpp
@@ -5,11 +5,11 @@
 
 class VFile : public VObject {
 private:
-    FILE* file{};
+    FILE* file;
     std::optional<std::string> filename_;
     std::optional<std::string> mode_;
 public:
     explicit VFile(const std::optional<std::string>& filename = std::nullopt, const std::optional<std::string>& mode = "r");
     ~VFile();
-    static void LoadFile(const string& filePath, const char* mode);
+    static void LoadFile(const std::string& filePath, const char* mode);
 };
